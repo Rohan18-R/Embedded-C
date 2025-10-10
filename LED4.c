9605fd70305d434759137ab2647b731b40c9307d
Rohan R <rohanv25r4@gmail.com>
2025-10-10 17:18:13 +0530

    Add files via upload

---
+++ LED4.c
@@ -0,0 +1,20 @@
+#include<lpc21xx.h>
+#include"delay.h"
+#define LED 0xff<<0
+int main()
+{
+ unsigned int i;
+ IODIR0=LED;
+ IOSET0=LED;
+ while(1)
+ {
+   for(i=0;i<8;i++)
+   {
+     IOCLR0=1<<i;
+	 delay(500);
+	 IOSET0=1<<i;
+	 delay(500);
+    }
+   }
+ }
+ 
